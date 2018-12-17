0bc00457b561f5f1d45e7587cc5f1729deff1ff6
Michalis Efstathiou <michalakis@users.noreply.github.com>
2018-12-17 01:59:01 +0100

    Fixed bugs and added/edited comments

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 /******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
+ A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
 /***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended:
-    - Add at least one `year` and/or `citation` property to at least one
-      quote object.
+  An array of objects containing quotes, as well as further
+  details about those quotes, such as the source, the year,
+  as well as the publications they appeared in, whenever
+  such information is applicable.
 ***/
 
 const quotes = [
@@ -22,8 +15,8 @@ const quotes = [
     source: "Norm Schryer"
   },
   {
-    quote: "It was a joke, okay?  If we thought it would actually be used, we wouldn’t have written it!",
-    source: "Mark Andreesen, speaking of the HTML tag BLINK"
+    quote: "It was a joke, okay? If we thought it would actually be used, we wouldn’t have written it!",
+    source: "Mark Andreesen, speaking of the html tag blink"
   },
   {
     quote: "Perl: The only language that looks the same before and after RSA encryption.",
@@ -50,15 +43,15 @@ const quotes = [
 
 
 /***
-  Create the `getRandomQuote` function to:
-   - generate a random number
-   - use the random number to `return` a random quote object from the
+  getRandomQuote function:
+   - generates a random number
+   - uses the random number to `return` a random quote object from the
      `quotes` array.
 ***/
 
 const getRandomQuote = array => {
 
-  var randomNumber = Math.floor( Math.random() * array.length );
+  const randomNumber = Math.floor( Math.random() * array.length );
 
   return array[randomNumber];
 
@@ -68,39 +61,34 @@ const getRandomQuote = array => {
 
 
 /***
-  Create the `printQuote` function to:
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before
+  printQuote function:
+   - calls the `getRandomQuote` function and assigns it to a variable.
+   - uses the properties of the quote object stored in the variable to
+     create an HTML string.
+   - uses conditionals to make sure the optional properties exist before
      they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string.
+   - sets the `innerHTML` of the `quote-box` div to the HTML string.
 ***/
 
 const printQuote = () => {
 
-  let quote = getRandomQuote();
+  let quote = getRandomQuote(quotes);
 
   let html = `
-    <p class="quote"> ${ quote.quote } </p>
-    <p class="source">
-      ${ quote.source }
-      ${
-        () => {
-          if ( quote.citation ) {
-          return `<span class="citation">, ${ quote.citation },</span>`
-          }
-        }
-       }
-       ${
-         () => {
-           if ( quote.year ) {
-           return `<span class="year">, ${ quote.year },</span>`
-           }
-         }
-        }
-    </p>
-  `;
+    <p class="quote">${quote.quote}</p>
+    <p class="source">${quote.source}`;
+
+  if ( quote.citation ) {
+    html += `<span class="citation">${quote.citation}</span>`;
+  }
+
+  if ( quote.year ) {
+    html += `<span class="year">${quote.year}</span>  `;
+  }
+
+  html += `</p>`;
+
+  document.getElementById('quote-box').innerHTML = html;
 
 };
 
@@ -108,11 +96,7 @@ const printQuote = () => {
 /***
   When the "Show another quote" button is clicked, the event listener
   below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
+  function.
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
